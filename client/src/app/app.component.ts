92d60816146e61fcc00df6dd51e6a5cefe3f4b2a
uvidzadanje <davidmiskeljin@gmail.com>
2022-08-26 23:13:38 +0200

    Dodato preuzimanje informacija za svaku rutu

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs';
+import { AppState } from './app.state';
+import { getAuthInfo } from './state/auth/auth.action';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(
+    private router: Router,
+    private store: Store<AppState>
+  )
+  {
+    this.router.events.pipe(filter(event => event !instanceof NavigationEnd)).subscribe(event => {
+      const token = localStorage.getItem("token");
+      if(!token) return;
+
+      this.store.dispatch(getAuthInfo({accessToken: token}))
+    })
+  }
 }
