7896ba8192c4f3fef9924115262a8c815c59abe1
uvidzadanje <davidmiskeljin@gmail.com>
2022-08-26 23:18:28 +0200

    Dodat state za tip zaduzenja

--- client/src/app/app.state.ts
+++ client/src/app/app.state.ts
@@ -1,4 +1,5 @@
 import { AuthState } from "./state/auth/auth.reducer";
+import { EmployeeTypeState } from "./state/employee-type/employee-type.reducer";
 import { EmployeeState } from "./state/employee/employee.reducer";
 import { ProjectState } from "./state/project/project.reducer";
 import { ResponsibilityState } from "./state/responsibility/responsibility.reducer";
@@ -10,4 +11,5 @@ export interface AppState {
   projects: ProjectState;
   responsibilites: ResponsibilityState;
   employees: EmployeeState;
+  employeeType: EmployeeTypeState
 }
