2ba04efcd4289a9a1d46feaaa20f8ad3c3952fb6
uvidzadanje <davidmiskeljin@gmail.com>
2022-08-26 22:45:40 +0200

    Dodat efekt i selektor umesto poziva servisa za dobijanje svih tipova zaposlenih

--- client/src/app/components/auth/register/register.component.ts
+++ client/src/app/components/auth/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
+import { AppState } from 'src/app/app.state';
 import { CreateEmployeeDto } from 'src/app/dto/employee/employee.dto';
 import { EmployeeType } from 'src/app/models/employee-type';
 import { AuthService } from 'src/app/services/auth.service';
 import { EmployeeTypeService } from 'src/app/services/employee-type.service';
+import { loadEmployeeTypes } from 'src/app/state/employee-type/employee-type.action';
+import { selectEmployeeTypes } from 'src/app/state/employee-type/employee-type.selector';
 
 @Component({
   selector: 'app-register',
@@ -22,12 +26,13 @@ export class RegisterComponent implements OnInit {
 
   title = 'Register';
   constructor(
-    private employeeTypeService: EmployeeTypeService,
+    private store: Store<AppState>,
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    this.employeeTypes =  this.employeeTypeService.getAll();
+    this.store.dispatch(loadEmployeeTypes());
+    this.employeeTypes =  this.store.select(selectEmployeeTypes);
   }
 
   register()
