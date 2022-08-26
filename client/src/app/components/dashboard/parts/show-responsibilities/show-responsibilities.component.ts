f24446b5b637a982d5824f0d458eb168c006e342
uvidzadanje <davidmiskeljin@gmail.com>
2022-08-26 22:58:46 +0200

    Modifikovana komponenta kao posebna stranica

--- client/src/app/components/dashboard/parts/show-responsibilities/show-responsibilities.component.ts
+++ client/src/app/components/dashboard/parts/show-responsibilities/show-responsibilities.component.ts
@@ -1,11 +1,14 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { skip, take } from 'rxjs';
 import { AppState } from 'src/app/app.state';
-import { CreateResponsibilityDto } from 'src/app/dto/responsibility/responsibility.dto';
+import { CreateResponsibilityDto, UpdateResponsibilityDto } from 'src/app/dto/responsibility/responsibility.dto';
 import { Responsibility } from 'src/app/models/responsibility';
+import { getAuthInfo } from 'src/app/state/auth/auth.action';
 import { selectAuthToken } from 'src/app/state/auth/auth.selector';
-import { addResponsibility, deleteResponsibility, getResponsibilityByProjectAndTeam } from 'src/app/state/responsibility/responsibility.action';
+import { loadEmployeesByTeam, setSelectedEmployeeIds } from 'src/app/state/employee/employee.action';
+import { addResponsibility, deleteResponsibility, getResponsibilityByProjectAndTeam, updateResponsibility } from 'src/app/state/responsibility/responsibility.action';
 import { selectResponsibilities } from 'src/app/state/responsibility/responsibility.selector';
 
 @Component({
@@ -18,25 +21,44 @@ export class ShowResponsibilitiesComponent implements OnInit {
   responsibilities: Responsibility[] = [];
   token: string = "";
 
+  paramsId = {
+    projectId: 0,
+    teamId: 0
+  }
+
   constructor(
-    public dialogRef: MatDialogRef<ShowResponsibilitiesComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {teamId: number, projectId: number},
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.store.select(selectAuthToken).subscribe(data => this.token = data);
-    this.store.dispatch(getResponsibilityByProjectAndTeam({...this.data, token: this.token}));
+    this.route.params.subscribe(params => {
+      this.paramsId.projectId = +params["projectId"];
+      this.paramsId.teamId = +params["teamId"];
+    })
 
-    this.store.select(selectResponsibilities).subscribe(data => this.responsibilities = data);
+    const token = localStorage.getItem("token");
+    if(token) this.store.dispatch(getAuthInfo({ accessToken: token}));
+
+
+    this.store.select(selectAuthToken).pipe(skip(1), take(1)).subscribe(data => {
+      this.token = data;
+      this.store.dispatch(getResponsibilityByProjectAndTeam({...this.paramsId, token: this.token}));
+    });
+
+
+    this.store.select(selectResponsibilities).pipe(skip(1), take(1)).subscribe(() => {
+      this.store.dispatch(loadEmployeesByTeam({teamId: this.paramsId.teamId}));
+    });
+
+    this.store.select(selectResponsibilities).pipe(skip(1)).subscribe(data => {
+      this.responsibilities = data;
+      this.store.dispatch(setSelectedEmployeeIds({employeeIds: this.getEmployeeIds()}));
+    });
+
   }
 
-  close()
-  {
-    this.dialogRef.close();
-  }
-
-  removeResponsibility(id: number)
+  remove({id}: {id: number})
   {
     this.store.dispatch(deleteResponsibility({id, token: this.token}));
   }
@@ -44,6 +66,11 @@ export class ShowResponsibilitiesComponent implements OnInit {
   add(responsibility: CreateResponsibilityDto)
   {
     this.store.dispatch(addResponsibility({responsibility: {...responsibility}, token: this.token}));
+  }
+
+  update({id, changes}: {id: number, changes: UpdateResponsibilityDto})
+  {
+    this.store.dispatch(updateResponsibility({id, changes, token: this.token}));
   }
 
   getEmployeeIds()
